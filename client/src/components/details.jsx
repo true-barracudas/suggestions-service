64db4937aa182671e2a90f7a35a1e7c11e2c9492
AmeliaYeoh <ameyjm@gmail.com>
2020-11-17 20:16:53 -0800

    Rendered sale prices

--- client/src/components/details.jsx
+++ client/src/components/details.jsx
@@ -2,10 +2,25 @@ import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
-const StyledText = styled.div`
+const StyledType = styled.div`
   font-family: AdihausDIN;
   font-size: 13px;
   padding: 3px;
+`;
+
+const StyledPrice = styled.div`
+  font-family: AdihausDIN;
+  font-size: 13px;
+  padding: 3px;
+  color: ${(props) => (props.salePrice !== 0 ? 'grey' : 'black')};
+  text-decoration: ${(props) => (props.salePrice !== 0 ? 'line-through' : 'none')};
+`;
+
+const StyledSalePrice = styled.div`
+  font-family: AdihausDIN;
+  font-size: 13px;
+  padding: 3px;
+  color: red;
 `;
 
 const StyledSeries = styled.div`
@@ -17,14 +32,33 @@ const StyledSeries = styled.div`
   margin-bottom: 5px;
 `;
 
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
 function Details({
   price, recycled, salePrice, series, type,
 }) {
+  console.log(typeof salePrice);
+  const getPrice = () => {
+    if (salePrice !== 0) {
+      return (
+        <Wrapper>
+          <StyledSalePrice>${salePrice}</StyledSalePrice>
+          <StyledPrice salePrice={salePrice}>${price}</StyledPrice>
+        </Wrapper>
+      );
+    }
+    return (
+      <StyledPrice salePrice={salePrice}>${price}</StyledPrice>
+    );
+  };
+  const itemPrice = getPrice();
   return (
     <>
       <StyledSeries>{series}</StyledSeries>
-      <StyledText>{type}</StyledText>
-      <StyledText>${price}</StyledText>
+      <StyledType>{type}</StyledType>
+      {itemPrice}
     </>
   );
 }
